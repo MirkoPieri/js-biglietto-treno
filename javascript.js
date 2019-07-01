6d86fe300d8dd29fff0244af5fd99b998f7328c0
MirkoPieri <mirko97.pieri@gmail.com>
2019-07-01 16:17:31 +0200

    Aggiunta vari commenti.

--- javascript.js
+++ javascript.js
@@ -2,11 +2,14 @@
 // Il programma dovrà chiedere all’utente il numero di chilometri e l’età e dovrà calcolare il prezzo totale del viaggio.
 // Il prezzo del biglietto è definito in base ai km (0.21 €cent al km) ma c’è uno sconto del 20% per i minorenni e del 40% per gli over 65.
 
+
+// definizione variabili
 var elemento = document.getElementById("mio_id");
 var elementoSconto = document.getElementById("sconto");
 
 var kilometri, eta, prezzoTotale, prezzoAlkm, prezzoSconto, prezzoFinale;
 
+// definizione vari interazioni dell'utente
 kilometri = parseInt(prompt("Inserisci i kilometri che devi percorrere"));
 
 eta = parseInt(prompt("Inserisci la tua età"));
@@ -16,16 +19,17 @@ prezzoAlkm = ("0.21");
 prezzoTotale = kilometri * prezzoAlkm;
 console.log(prezzoTotale);
 
+// ciclo if per sconto boglietto
 if (eta < 18) {
- prezzoSconto = ((prezzoTotale * 20) / 100);
- prezzoFinale = prezzoTotale - prezzoSconto;
- elemento.innerHTML = prezzoFinale + "€";
- elementoSconto.innerHTML = "Sconto del 20% perché minorenne.";
+   prezzoSconto = ((prezzoTotale * 20) / 100);
+   prezzoFinale = prezzoTotale - prezzoSconto;
+   elemento.innerHTML = prezzoFinale + "€";
+   elementoSconto.innerHTML = "Sconto del 20% perché minorenne.";
 } else if (eta > 65) {
-  prezzoSconto = ((prezzoTotale * 40) / 100);
-  prezzoFinale = prezzoTotale - prezzoSconto;
-  elemento.innerHTML = prezzoFinale + "€";
-  elementoSconto.innerHTML = "Sconto del 40% perché over 65 anni.";
+    prezzoSconto = ((prezzoTotale * 40) / 100);
+    prezzoFinale = prezzoTotale - prezzoSconto;
+    elemento.innerHTML = prezzoFinale + "€";
+    elementoSconto.innerHTML = "Sconto del 40% perché over 65 anni.";
 } else {
-  elemento.innerHTML = prezzoTotale + "€";
+    elemento.innerHTML = prezzoTotale + "€";
 }
